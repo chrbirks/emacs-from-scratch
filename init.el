40e1dc95673a6ddd8796444362abfee47f07383d
Christian Birk Sørensen <chrbirks@gmail.com>
2023-05-10 21:15:16 +0200

    Added lsp-mode config

--- init.el
+++ init.el
@@ -1140,27 +1140,106 @@ _b_: browse packages _q_: quit
   :ensure t
   :defer t)
 
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
 
-;; (defun efs/lsp-mode-setup ()
-;;   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-;;   (lsp-headerline-breadcrumb-mode))
+(use-package lsp-mode
+  :defer t
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . efs/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  ;; Optimize parameters for lsp-mode
+  (setq lsp-enable-file-watchers t
+        lsp-file-watch-threshold 100000000
+        read-process-output-max (* 1024 1024) ;; 1mb
+        lsp-idle-delay 0.500)
+  ;; Other lsp settings
+  (setq ; Show info box
+        lsp-ui-doc-enable t
+        lsp-ui-doc-show-with-cursor nil
+        lsp-ui-doc-show-with-mouse t
+        lsp-ui-doc-header t
+        lsp-ui-doc-include-signature t
+        lsp-ui-doc-position 'at-point; 'top, 'bottom or 'at-point
+        lsp-ui-doc-alignment 'window ; 'frame or 'window
+        lsp-ui-doc-border "white"
+        ;; lsp-ui-doc-max-width 150
+        lsp-ui-doc-max-height 8
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-use-webkit nil ;; Use lsp-ui-doc-webkit only in GUI. Requires compiling --with-xwidgets
+        ; Show info from selected line on the same line
+        lsp-ui-sideline-enable nil
+        lsp-ui-sideline-show-symbol t
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-sideline-show-code-actions nil ; Show all possible LSP actions such as renaming, type casting, etc.
+        ; Enable lenses
+        lsp-lens-enable t
+        ;; lsp-lens-place-position ; FIXME (05-09-2021) Not implemented yet
+        ; Eldoc
+        lsp-eldoc-enable-hover nil ; Show LSP info in minibuffer?
+        lsp-eldoc-enable-signature-help t
+        lsp-eldoc-prefer-signature-help t
+        lsp-eldoc-render-all t
+        ; Modeline
+        lsp-modeline-workspace-status-enable t
+        lsp-modeline-code-actions-enable t
+        lsp-modeline-diagnostics-enable t
+        ; Auto completion
+        lsp-completion-provider :capf
+        lsp-completion-show-detail t
+        lsp-completion-show-kind t
+        lsp-completion-enable t
+        lsp-completion-enable-additional-text-edit t
+        ; Headerline
+        lsp-headerline-breadcrumb-mode t
+        lsp-headerline-breadcrumb-enable t
+        ; SignatureHelp
+        lsp-signature-render-all t
+        lsp-signature-auto-activate t
+        lsp-signature-render-documentation t
+        lsp-signature-function 'lsp-signature-posframe ; Use posframe with SignatureHelp. default: lsp-lv-message
+        ; Other options
+        lsp-use-upstream-bindings t ; Bind all upstream managed `lsp-command-map` bindings behind `SPC m`. See https://emacs-lsp.github.io/lsp-mode/page/keybindings/
+        lsp-enable-symbol-highlighting nil
+        company-lsp-cache-candidates 'auto
+        lsp-ui-imenu-enable t ;TODO 17-05-2019: Does not work. Should call lsp-ui-imenu which works
+        lsp-enable-imenu t
+        lsp-ui-imenu-auto-refresh t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-always-show nil
+        lsp-prefer-flymake nil ; 't' (flymake), 'nil' (flycheck), ':none' (None of them)
+        lsp-ui-flycheck-enable t
+        lsp-ui-flycheck-list-position 'bottom
+        lsp-ui-flycheck-live-reporting t
+        lsp-auto-configure t ; auto-configure lsp-ui and company-lsp
+        lsp-auto-guess-root nil
+        lsp-lens-mode t
+        lsp-enable-indentation t
+        lsp-enable-snippet t
+        lsp-enable-on-type-formatting nil
+        lsp-enable-file-watchers t
+        lsp-enable-xref t
+        lsp-log-io nil ; log all messages to *lsp-log* for debugging
+        lsp-print-performance nil ; check lsp-log data
+        lsp-server-trace nil ; request tracing on the server side
+        )
+  (lsp-enable-which-key-integration t))
 
-;; (use-package lsp-mode
-;;   :defer t
-;;   :commands (lsp lsp-deferred)
-;;   :hook (lsp-mode . efs/lsp-mode-setup)
-;;   :init
-;;   (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
-;;   :config
-;;   (lsp-enable-which-key-integration t))
+;; See all error statistics in modeline
+(with-eval-after-load 'lsp-mode
+  (setq lsp-modeline-diagnostics-scope :project) ;project, workspace or file
+  (add-hook 'lsp-managed-mode-hook 'lsp-modeline-diagnostics-mode))
 
-;; (use-package lsp-ui
-;;   :hook (lsp-mode . lsp-ui-mode)
-;;   :custom
-;;   (lsp-ui-doc-position 'bottom))
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
 
-;; (use-package lsp-treemacs
-;;   :after lsp)
+(use-package lsp-treemacs
+  :after lsp)
 
 ;; (use-package dap-mode
 ;;   ;; Uncomment the config below if you want all UI panes to be hidden by default!
