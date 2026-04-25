1212893a8b68c412b04cb563db5c8418d9180b04
Christian Birk Sørensen <chrbirks@gmail.com>
2026-04-25 11:51:37 +0200

    Fixed consult commands that were renamed recently

--- init.el
+++ init.el
@@ -775,8 +775,8 @@ Plays nice with special buffers like treemacs."
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
 
